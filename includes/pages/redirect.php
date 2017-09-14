f0e2c4a79a39a7e766e998df1c992c9e45ee10a5
Nicholas Headlong <nicholas.headlong@aesopagency.com>
2017-09-14 09:14:10 +0100

    update to gateway

--- includes/pages/redirect.php
+++ includes/pages/redirect.php
@@ -1,21 +1,21 @@
 <?php
-    header( 'Access-Control-Allow-Origin: '.get_site_url() );
-    header( 'X-Frame-Options: SAMEORIGIN' );
-    header( 'Access-Control-Allow-Credentials: false' );
+    //prevents XSS injecting malicious html or js into redirect
+    $safeDecodedUrl = preg_replace('/([\"\>\<])/', '', urldecode($_GET["page"]));
+    // prevents form action hijacking
+    if (strpos($_GET["page"], urlencode($_SERVER['HTTP_HOST'])) !== false) {
 
-    $safeDecodedUrl = esc_url_raw( $_GET["page"] );
+            $res = '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">' .
+                '<html><head>' .
+                '<script type="text/javascript"> function OnLoadEvent() { document.form.submit(); }</script>' .
+                '<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />' .
+                '<title>3D-Secure Redirect</title></head>' .
+                '<body OnLoad="OnLoadEvent();">' .
+                '<form name="form" action="' . $safeDecodedUrl . '" method="POST"  target="_top" >' .
+                '<noscript>' .
+                '<center><p>Please click button below to Authenticate your card</p><input type="submit" value="Go"/></p></center>' .
+                '</noscript>' .
+                '</form></body></html>';
 
-    $res =  '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">' .
-            '<html><head>' .
-            '<script type="text/javascript"> function OnLoadEvent() { document.form.submit(); }</script>' .
-            '<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />' .
-            '<title>3D-Secure Redirect</title></head>' . 
-            '<body OnLoad="OnLoadEvent();">' .
-            '<form name="form" action="'. $safeDecodedUrl . '" method="POST"  target="_top" >' .
-            '<noscript>' .
-            '<center><p>Please click button below to Authenticate your card</p><input type="submit" value="Go"/></p></center>' .
-            '</noscript>' .
-            '</form></body></html>';
-            
-    echo $res;
+            echo $res;
+    }
 ?>
